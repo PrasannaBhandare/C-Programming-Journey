e526c67741dea25028c1d5921e3e02c8d923e09e
PrasannaBhandare <prasannabhandare32@gmail.com>
2026-08-15 16:12:45 +0530

    Add array print program

---
+++ 03_Arrays/01_print_array.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+ int a[5]={10,20,30,40,50};
+ int i;
+ printf("array elements:\n");
+ for(i=0;i<5;i++)
+{printf("inedx=%d ele=%d ",i,a[i]);
+ printf("\n");
+}
+return 0;
+}
